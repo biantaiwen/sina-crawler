200d620dde226a5a2e54587c3e00cd3c8c1ab045
biantaiwen <biantaiwen@163.com>
2020-10-27 18:56:26 +0800

    sync code

--- src/main/java/com/qxz/sina/crawler/Main.java
+++ src/main/java/com/qxz/sina/crawler/Main.java
@@ -12,37 +12,78 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.regex.Pattern;
 
 public class Main {
-
+    private static final String USER_NAME = "root";
+    private static final String USER_PASSWORD = "root";
+    private static final String DATABASE_URL = "jdbc:h2:file:/Users/guoxiaodong/Desktop/btw_test/java/sina-crawler/db/news";
     public static void main(String[] args) {
-        // https://sina.cn
-        String url = "https://sina.cn";
-//        String url = "https://sports.sina.cn/china/2020-10-26/detail-iiznctkc7803967.d.html?vt=4&pos=108";
-        ArrayList<String> unHandleUrl = new ArrayList<>(Collections.singletonList(url));
-        Set<String> handleUrl = new HashSet<>();
-        // https://sports.sina.cn/china/2020-10-26/detail-iiznctkc7803967.d.html?vt=4&pos=108
-        while (true) {
-            if (unHandleUrl.isEmpty()) {
-                break;
-            }
-            String link = unHandleUrl.remove(unHandleUrl.size() - 1);
-            if (!handleUrl.contains(link)) {
-                handleUrl.add(link);
-                System.out.println("link 1 = " + link);
-                Document document = getHtmlAndParseByUrl(link);
-                Set<String> newsHrefAll = getPageNewsHrefAll(document);
-                unHandleUrl.addAll(newsHrefAll);
-                parseNewsDetailAndSave(document);
-            }
+        Connection databaseConnection = createDatabaseConnection();
+        List<String> unHandleUrl = selectUnHandleUrl(databaseConnection);
 
+
+
+//        String url = "https://sina.cn";
+////        String url = "https://sports.sina.cn/china/2020-10-26/detail-iiznctkc7803967.d.html?vt=4&pos=108";
+//        ArrayList<String> unHandleUrl = new ArrayList<>(Collections.singletonList(url));
+//        Set<String> handleUrl = new HashSet<>();
+//        // https://sports.sina.cn/china/2020-10-26/detail-iiznctkc7803967.d.html?vt=4&pos=108
+//        while (true) {
+//            if (unHandleUrl.isEmpty()) {
+//                break;
+//            }
+//            String link = unHandleUrl.remove(unHandleUrl.size() - 1);
+//            if (!handleUrl.contains(link)) {
+//                handleUrl.add(link);
+//                System.out.println("link 1 = " + link);
+//                Document document = getHtmlAndParseByUrl(link);
+//                Set<String> newsHrefAll = getPageNewsHrefAll(document);
+//                unHandleUrl.addAll(newsHrefAll);
+//                parseNewsDetailAndSave(document);
+//            }
+//
+//        }
+
+    }
+    private static void insertIntoUnHandleUrl(Connection databaseConnection,String url) {
+        try {
+            PreparedStatement statement = databaseConnection.prepareStatement("SELECT * FROM UN_HANDLE_URL;");
+            ResultSet resultSet = statement.executeQuery();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
         }
+    }
+    private static List<String> selectUnHandleUrl(Connection databaseConnection) {
+        List<String> stringList = new ArrayList<>();
+        try {
+            PreparedStatement statement = databaseConnection.prepareStatement("SELECT * FROM UN_HANDLE_URL;");
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()){
+                stringList.add(resultSet.getString(1));
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return stringList;
+    }
 
+    private static Connection createDatabaseConnection() {
+        try {
+            return DriverManager.getConnection(DATABASE_URL, USER_NAME, USER_PASSWORD);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+            throw new RuntimeException(throwables);
+        }
     }
 
     private static Document getHtmlAndParseByUrl(String url) {
